694fbc14ba2441ad5c850230a3647d2553cecf08
Nardis <nardis9501@gmail.com>
2023-09-10 13:51:33 -0400

    refactor: improve styles in conditional rendering

--- src/assets/components/Square.jsx
+++ src/assets/components/Square.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-export default function Square({ children, index, noBorder }) {
+export default function Square({
+  children,
+  index,
+  noBorder,
+  updateBoard,
+  isSelected,
+  noHover,
+}) {
+  const seleted = `${isSelected ? "bg-blue-400 " : ""}`; //'rounded-xl bg-blue-400'
+  const hover = `${noHover ? " " : "hover:bg-slate-400/40"}`;
+  const border = `${
+    noBorder
+      ? ""
+      : "border-solid border-2 border-y-sky-600 border-x-sky-300 cursor-pointer"
+  }`;
+  const handlerClick = () => {
+    updateBoard && updateBoard(index);
+  };
   return (
     <div
-      className={
-        noBorder
-          ? "grid text-3xl place-items-center  h-28 w-28 cursor-pointer"
-          : "SQUARE grid text-3xl place-items-center border-solid border-2 border-y-sky-600 border-x-sky-300 rounded-md h-28 w-28 cursor-pointer"
-      }
+      onClick={handlerClick}
+      className={`grid text-3xl place-items-center rounded-lg md:h-28 md:w-28 sm:h-24 sm:w-24 h-16 w-16  ${seleted} ${border} ${hover}`}
     >
       {children}
     </div>
